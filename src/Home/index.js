12b38e0c94c28aaf5fa661e5683c8332c1481711
Romain Lafosse <romain75l@gmail.com>
2021-08-25 18:23:58 +0200

    New picture added on the home page / CSS modified for media queries.

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,13 @@
+//Import React and packages
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 
+//Import Local datas
+import MyPicture from '../assets/img/photo-romain01.jpg';
+
 const Home = () => {
+
+
 
   return (
     <div className="home">
@@ -9,9 +15,17 @@ const Home = () => {
         <h2 className="home__title">Romain Lafosse</h2>
         <h3 className="home__title__description">Développeur web</h3>
       </Fade>
-      <Fade bottom>
-        <h1 className="home__title__specialisation">Développeur Javascript Fullstack spécialisé en ReactJS.</h1>
-      </Fade>
+      <div className="home__title__specialisation">
+        <div className="home__title__specialisation__content">
+          <Fade bottom>
+            <h1 className="home__title__specialisation__content__text">Développeur Javascript Fullstack spécialisé en ReactJS.</h1>
+          </Fade>
+        </div>
+        <div className="home__title__specialisation__content__img">
+          <img className="home__title__specialisation__content__img__picture" src={MyPicture} alt="Romain Lafosse Développeur" />
+        </div>
+
+      </div>
     </div>
   );
 };
