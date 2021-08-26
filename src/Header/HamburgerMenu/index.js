1f45f50f9287b7b1c9c82a033f2c1521c85f3eee
Romain Lafosse <romain75l@gmail.com>
2021-08-26 17:29:26 +0200

    Hamburger menu and links work

--- src/Header/HamburgerMenu/index.js
+++ src/Header/HamburgerMenu/index.js
@@ -8,21 +8,18 @@ const HamburgerMenu = () => {
   const handleShowHamburgerMenu = () => setHamburgerMenu(!isHamburgerMenu);
 
   return (
-    <nav className="hamburger">
-      {isHamburgerMenu && (
-        <div className="hamburger__links">
-          <NavLink to="/about" className="hamburger__links__link">Qui-suis-je ?</NavLink>
-          <NavLink to="/resume" className="hamburger__links__link">Mon CV</NavLink>
-          <NavLink to="/projects" className="hamburger__links__link">Mes projets</NavLink>
-          <NavLink to="/contact" className="hamburger__links__link">Contact</NavLink>
-        </div>
-      )}
-
+    <nav className={`hamburger ${isHamburgerMenu ? "show-hamburger" : "hide-hamburger"} `}>
+      <div className="hamburger__links">
+        <NavLink onClick={handleShowHamburgerMenu} to="/about" className="hamburger__links__link">Qui-suis-je ?</NavLink>
+        <NavLink onClick={handleShowHamburgerMenu} to="/resume" className="hamburger__links__link">Mon CV</NavLink>
+        <NavLink onClick={handleShowHamburgerMenu} to="/projects" className="hamburger__links__link">Mes projets</NavLink>
+        <NavLink onClick={handleShowHamburgerMenu} to="/contact" className="hamburger__links__link">Contact</NavLink>
+      </div>
       <button className="hamburger__button"
         onClick={handleShowHamburgerMenu}>
         <span className="hamburger__button__bar"></span>
       </button>
-    </nav>
+    </nav >
   );
 };
 
